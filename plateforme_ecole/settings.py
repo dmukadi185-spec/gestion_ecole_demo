0100641591b2ee480bbae64756bf2829269e8a85
dmukadi185 <dmukadi185-spec@example.com>
2026-05-28 23:51:08 +0200

    Add STATIC_ROOT for collectstatic

--- plateforme_ecole/settings.py
+++ plateforme_ecole/settings.py
@@ -77,8 +77,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = "accounts:signin"
 LOGIN_REDIRECT_URL = "dashboard:index"
